55f3b93067a92bdea7507802320612ed2023b8a4
ValentinePumpkins <quignon.val@gmail.com>
2023-10-31 15:10:52 +0100

    FileStorage : creation Co-authored-by: Electrik <electrikbox@users.noreply.github.com>

---
+++ models/engine/file_storage.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Defines FileStorage class """
+
+from models.base_model import BaseModel
+import json
+
+
+class FileStorage():
+    """ Represents a FileStorage class
+    Attributes:
+        __file_path (str): path to JSON file
+        __objects (dict): dictionary of objects
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ Show all object from __objects """
+        return self.__objects
+
+    def new(self, obj):
+        """ Add new object in __objects """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """ Serialize __objects to the JSON file """
+        data = {}
+        for key, value in self.__objects.items():
+            data[key] = value.to_dict()
+        with open(self.__file_path, "w", encoding='utf-8') as file:
+            json.dump(data, file, indent=4)
+
+    def reload(self):
+        """ Deserialize __objects from the JSON file"""
+        try:
+            with open(self.__file_path, "r", encoding='utf-8') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split(".")
+                    class_name = class_name.replace("_", " ")
+                    instance = eval(class_name)(**value)
+                    self.__objects[key] = instance
+        except FileNotFoundError:
+            pass
