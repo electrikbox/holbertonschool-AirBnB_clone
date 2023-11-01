d35abede017040c2fcabe93d889cec7dd6d25f08
electrikbox <electrikbox@icloud.com>
2023-11-01 15:45:06 +0100

    add tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,54 +1,16 @@
 #!/usr/bin/python3
-""" Unittest for BaseModel """
+""" Unittest for User """
 
 import unittest
 from models.base_model import BaseModel
 from models.user import User
-from datetime import datetime
-import os
 
 
 class testUser(unittest.TestCase):
     """ Test class for User """
 
-    def setUp(self):
-        """ Set up for test """
-        self.model = User()
-
-    # def test_init(self):
-    #     """ Test init """
-    #     self.assertIsInstance(self.model, User)
-    #     self.assertIsInstance(self.model.id, str)
-    #     self.assertIsInstance(self.model.created_at, datetime)
-    #     self.assertIsInstance(self.model.updated_at, datetime)
-
-    # def test_save(self):
-    #     """ Test save """
-    #     first_updated_at = self.model.updated_at
-    #     self.model.save()
-    #     self.assertNotEqual(first_updated_at, self.model.updated_at)
-    #     os.remove("file.json")
-
-    # def test_to_dict(self):
-    #     """ Test to dict"""
-    #     model_dict = self.model.to_dict()
-    #     self.assertIsInstance(model_dict, dict)
-    #     self.assertIn('id', model_dict)
-    #     self.assertIn('created_at', model_dict)
-    #     self.assertIn('updated_at', model_dict)
-    #     self.assertIn('__class__', model_dict)
-
-    # def test_str(self):
-    #     """ Test string rep """
-    #     model_str = str(self.model)
-    #     self.assertIsInstance(model_str, str)
-    #     self.assertIn('[User]', model_str)
-    #     self.assertIn('id', model_str)
-    #     self.assertIn(str(self.model.id), model_str)
-    #     self.assertIn(str(self.model.__dict__), model_str)
-
     def test_class(self):
-        """ Validate the types of the attributes an class. """
+        """ Validate attributes type """
         with self.subTest(msg='Inheritance'):
             self.assertTrue(issubclass(User, BaseModel))
 
