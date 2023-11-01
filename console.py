442d1892748a00d54dcbfc4b416982d2739620be
electrikbox <electrikbox@icloud.com>
2023-11-01 11:57:09 +0100

    add methods (create, show, destroy, all, update)
    Co-authored-by: ValentinePumpkins <ValPumpkins@users.noreply.github.com>

--- console.py
+++ console.py
@@ -2,6 +2,8 @@
 """ Console class """
 
 import cmd
+import shlex
+import models
 
 
 class HBNBCommand(cmd.Cmd):
@@ -23,6 +25,140 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing when user input is empty"""
         pass
+
+    def do_create(self, arg):
+        """ Create an instance and save it """
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = shlex.split(arg)
+        class_name = args[0]
+
+        if class_name not in models.classes:
+            print("** class doesn't exist **")
+            return
+
+        instance = getattr(models, class_name)()
+        instance.save()
+        print(instance.id)
+
+    def do_show(self, arg):
+        """ Prints the string rep of an instance """
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = shlex.split(arg)
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        class_name = args[0]
+        instance_id = args[1]
+
+        if class_name not in models.classes:
+            print("** class doesn't exist **")
+            return
+
+        all_objects = models.storage.all()
+        key = "{}.{}".format(class_name, instance_id)
+
+        if key not in all_objects:
+            print("** no instance found **")
+            return
+
+        print(all_objects[key])
+
+    def do_destroy(self, arg):
+        """ Deletes an instance based on class name + ID """
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = shlex.split(arg)
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        class_name = args[0]
+        instance_id = args[1]
+
+        if class_name not in models.classes:
+            print("** class doesn't exist **")
+            return
+
+        all_objects = models.storage.all()
+        key = "{}.{}".format(class_name, instance_id)
+
+        if key not in all_objects:
+            print("** no instance found **")
+            return
+
+        del all_objects[key]
+        models.storage.save()
+
+    def do_all(self, arg):
+        """Print all instances from the storage"""
+        all_objects = models.storage.all()
+
+        if not arg:
+            print([str(instance) for instance in all_objects.values()])
+        else:
+            args = shlex.split(arg)
+            class_name = args[0]
+
+            if class_name not in models.classes:
+                print("** class doesn't exist **")
+                return
+
+            for instance in all_objects.values():
+                if type(instance).__name__ == class_name:
+                    print(str(instance))
+
+    def do_update(self, arg):
+        """Update an instance and save it"""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = shlex.split(arg)
+
+        class_name = args[0]
+
+        if class_name not in models.classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args[1]
+
+        all_objects = models.storage.all()
+        key = "{}.{}".format(class_name, instance_id)
+
+        if key not in all_objects:
+            print("** no instance found **")
+            return
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+
+        attribut_name = args[2]
+
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        attribut_value = args[3]
+
+        setattr(all_objects[key], attribut_name, attribut_value)
+        all_objects[key].save()
 
 
 if __name__ == '__main__':
